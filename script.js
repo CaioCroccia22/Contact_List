1a549b6f9d8ac2ca5b5eed1594f7522b7100d8f2
caiocroccia22 <caiocroccia22@gmail.com>
2024-08-30 00:02:56 -0300

    feat: addTable function

--- script.js
+++ script.js
@@ -4,5 +4,17 @@ const contactForm = document.getElementById('contact-form');
 
 contactForm.addEventListener('submit', function(e){
     e.preventDefault();
-    console.log('teste inicial');
+    const nameInput = document.getElementById('name');
+    const phoneInput = document.getElementById('phone_number');
+    alert(`Você inseriu na agenda o contato: ${nameInput.value} de número: ${phoneInput.value}`)
+    addTable(nameInput.value, phoneInput.value);
 });
+
+function addTable(name, phone){
+    const tableRow = document.getElementById('table-row');
+    tableRow.innerHTML += `<tr>
+        <td>${name}</td>
+        <td>${phone}</td>
+    </tr>'`
+
+}
