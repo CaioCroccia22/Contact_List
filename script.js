5764e810f3b03d6ea2534dd74dc0107d20db7afb
caiocroccia22 <caiocroccia22@gmail.com>
2024-08-29 22:05:56 -0300

    feat: form event to test js

---
+++ script.js
@@ -0,0 +1,8 @@
+// console.log('teste inicial');
+
+const contactForm = document.getElementById('contact-form');
+
+contactForm.addEventListener('submit', function(e){
+    e.preventDefault();
+    console.log('teste inicial');
+});
